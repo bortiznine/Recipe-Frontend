e1715ce6d0bcfdba2bd08dd418da49b323056660
Brian Ortiz <bortiznine@gmail.com>
2021-05-10 15:32:36 -0500

    added login to app

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {UserService} from 'src/app/services/user/user.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public email: string;
+  public password: string;
+  constructor(private userService: UserService) { }
 
-  constructor() { }
+  loginUser(): void {
+    const user = {email: this.email, password: this.password};
+    console.log(user);
+    this.userService.loginUser(user);
+  }
 
   ngOnInit(): void {
   }
