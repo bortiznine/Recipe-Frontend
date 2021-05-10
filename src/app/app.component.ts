3d0cb2276fdd2b77f429c958a0cb3df27079208c
Brian Ortiz <bortiznine@gmail.com>
2021-05-10 14:25:15 -0500

    Changed title for app component as well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-recipes-frontend';
+  title = 'angular-recipes-frontend-title-update';
 }
